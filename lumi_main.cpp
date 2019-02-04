730e6983c55df6ecd4a7fc462f2d5c68773de588
zelinliu2 <46951816+zelinliu2@users.noreply.github.com>
2019-02-04 00:39:58 -0700

    Comments and closing dialogbox

--- lumi_main.cpp
+++ lumi_main.cpp
@@ -1,7 +1,22 @@
+/*
+ * File: lumi_main.cpp
+ * Created: 01/25/19
+ * Authors: Zelin Liu
+ * Content: This is the main file for Lumi-pins app.
+ *
+ *
+ * */
+
 #include <windows.h>
 #include <stdio.h>
 
+// Forward Declarations
 LRESULT CALLBACK LumiWndProc(HWND, UINT, WPARAM, LPARAM);
+void CloseProgram(HWND, bool);
+
+/*
+ * The main function for Windows application
+ */
 int WINAPI WinMain(HINSTANCE hInstance,
                    HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine,
@@ -9,28 +24,30 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 //  MessageBox(NULL, "Error", "Lumi-Pins", MB_OK);
 
-	// create a window class
 	const char CLASS_NAME[]  = "class0";
+
+	// create a window class
 	WNDCLASSEXA  wc0 = { };
-	wc0.cbSize = sizeof(WNDCLASSEX);
-	wc0.lpfnWndProc = LumiWndProc;
-	wc0.hInstance     = hInstance;
-	wc0.lpszClassName = CLASS_NAME;
-	wc0.style = CS_HREDRAW | CS_VREDRAW;
+	wc0.cbSize = sizeof(WNDCLASSEX);	// default size
+	wc0.lpfnWndProc = LumiWndProc;	// callback process
+	wc0.hInstance     = hInstance;	// instance handle
+	wc0.lpszClassName = CLASS_NAME;	// set class name
+	wc0.style = CS_HREDRAW | CS_VREDRAW;	// class style, you can OR these macros
 	wc0.cbClsExtra = 0;
 	wc0.cbWndExtra = 0;
-	wc0.lpszMenuName = "lumi_menu_rc";
-	wc0.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-	wc0.hCursor = LoadCursorA(NULL, IDC_ARROW);
-	wc0.hIcon = LoadIcon(NULL, IDI_APPLICATION); // to do: create an icon and link its handle
+	wc0.lpszMenuName = "lumi_menu_rc";	// set menu resource
+	wc0.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);	// background color
+	wc0.hCursor = LoadCursorA(NULL, IDC_ARROW);	//	default cursor
+	wc0.hIcon = LoadIcon(NULL, IDI_APPLICATION); // class icon ***PLANNED***
+
 	// register window class
-	if (!RegisterClassExA(&wc0)){
+	if (!RegisterClassExA(&wc0)){	// returns a class atom on success, 0 on failure
 		MessageBox(NULL, "Error: RegisterClassExA returned 0", "Lumi-Pins", MB_OK);
 		return 0;
 	}
 
 	// create window
-	HWND hwnd = CreateWindowExA(
+	HWND hwnd = CreateWindowExA(	// returns an handle
 			0, // Extra Window styles
 			CLASS_NAME,	// WNDCLASSEXA name
 			"LumiPins", // Top bar name
@@ -42,14 +59,15 @@ int WINAPI WinMain(HINSTANCE hInstance,
 			NULL // Passing value
 			);
 
-	// check window creation
+	// check window creation, handle would be null on failure
 	if (hwnd == NULL){
 		MessageBox(NULL, "Error: CreateWindowExA returned NULL", "Lumi-Pins", MB_OK);
 		return 0;
 	}
 
-	ShowWindow(hwnd, nCmdShow);
+	ShowWindow(hwnd, nCmdShow);	// show window
 
+	// sent message loop to the callback function
 	MSG msg = { };
 	while (GetMessage(&msg, NULL, 0, 0)){
 		TranslateMessage(&msg);
@@ -62,26 +80,33 @@ int WINAPI WinMain(HINSTANCE hInstance,
   return 0;
 }
 
+/*
+ * The call back function for Windows applcation
+ */
 LRESULT CALLBACK LumiWndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
 	switch(msg){
 	case WM_DESTROY:
-		PostQuitMessage(0);
+		PostQuitMessage(0);	// closes background thread
 		return 0;
-	case WM_COMMAND:
+	case WM_CLOSE:	// when X is clicked, or when the window is closed from task bar
+		CloseProgram(hwnd, 0);
+		return 0;
+	case WM_COMMAND:	//
 		switch(HIWORD(wParam)){
 		case 0:
 			switch(LOWORD(wParam)){
 			case 101:
-				MessageBox(NULL, "New", "Lumi-Pins", MB_OK);
+				MessageBox(NULL, "New", "Lumi-Pins", MB_OK);	// placeholder
 				break;
 			case 102:
-				MessageBox(NULL, "Save", "Lumi-Pins", MB_OK);
+				MessageBox(NULL, "Save", "Lumi-Pins", MB_OK);	// placeholder
 				break;
 			case 103:
-				MessageBox(NULL, "Load", "Lumi-Pins", MB_OK);
+				MessageBox(NULL, "Load", "Lumi-Pins", MB_OK);	// placeholder
 				break;
 			case 104:
-				PostQuitMessage(0);
+				CloseProgram(hwnd, 0);
+				break;
 			}
 			break;
 		case 1:
@@ -92,4 +117,21 @@ LRESULT CALLBACK LumiWndProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		return 0;
 	}
 	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+void CloseProgram(HWND hwnd, bool saved){
+	if(saved){
+		PostQuitMessage(0);
+	}else{
+		switch(MessageBox(hwnd, "You have unsaved work, would you like to save?", "Error", MB_YESNOCANCEL)){
+				case 6:	// Yes
+					// to be added: save function
+					break;
+				case 7:	// No
+					DestroyWindow(hwnd);	// manually destroys window, sends WM_DESTROY
+					break;
+				case 2:	// Cancel
+					break;
+				}
+	}
 }
